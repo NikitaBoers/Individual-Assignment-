fdaa740fb0490727dc5454a26e31d36ab2a8f64c
Nikita Boers <boers.nikita@gmail.com>
2019-01-17 16:22:14 +0100

    I added latitude and longitude for maps

--- processing_conflict.py
+++ processing_conflict.py
@@ -11,22 +11,24 @@ with open(filename) as file:
             russia.append(line['type_of_violence'])
             russia.append(line['country'])
             russia.append(line['year'])
+            russia.append(line['latitude'])
+            russia.append(line['longitude'])
             all_conflicts.append(russia)
         if 'Turkey' in country:
             turkey=[]
             turkey.append(line['type_of_violence'])
             turkey.append(line['country'])
             turkey.append(line['year'])
+            turkey.append(line['latitude'])
+            turkey.append(line['longitude'])
             all_conflicts.append(turkey)
             
 
 print(all_conflicts)
 
-
-
 import csv
 
-header = ['type_of_conflict', 'country', 'year']
+header = ['type_of_conflict', 'country', 'year', 'latitude', 'longitude']
 with open ('all_conflicts.csv','w') as file :
     writer = csv.writer(file , lineterminator ='\n', delimiter =',')
     writer.writerow(header)
