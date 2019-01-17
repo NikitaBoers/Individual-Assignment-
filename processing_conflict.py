1c9953db11aa4dea537450629458577c1b9c9bb8
Nikita Boers <boers.nikita@gmail.com>
2019-01-17 16:01:51 +0100

    Now makes list of lists

--- processing_conflict.py
+++ processing_conflict.py
@@ -1,33 +1,36 @@
 import json
 filename = 'conflict_data_full_lined.json'
 
-russia_conflicts=[]
-turkey_conflicts=[]
+all_conflicts=[]
 with open(filename) as file:
 
     for line in json.load(file):
         country=line['country']
         if 'Russia' in country:
-            type_of_violence_russia=line['type_of_violence']
-            russia_conflicts.append(type_of_violence_russia)
+            russia=[]
+            russia.append(line['type_of_violence'])
+            russia.append(line['country'])
+            russia.append(line['year'])
+            all_conflicts.append(russia)
         if 'Turkey' in country:
-            type_of_violence_turkey=line['type_of_violence']
-            turkey_conflicts.append(type_of_violence_turkey)
+            turkey=[]
+            turkey.append(line['type_of_violence'])
+            turkey.append(line['country'])
+            turkey.append(line['year'])
+            all_conflicts.append(turkey)
             
 
-print(russia_conflicts)
-print(turkey_conflicts)
+print(all_conflicts)
 
-with open ('russia_conflicts.csv ', 'w') as file :
-    file.write('russia')
-    for entry in russia_conflicts:
-        file.write(str(entry)) 
 
-with open ('turkey_conflicts.csv', 'w') as file:
-    file.write('turkey')
-    for entry in turkey_conflicts:
-        file.write(str(entry))
 
-        
+import csv
+
+header = ['type_of_conflict', 'country', 'year']
+with open ('all_conflicts.csv','w') as file :
+    writer = csv.writer(file , lineterminator ='\n', delimiter =',')
+    writer.writerow(header)
+    for conflict in all_conflicts :
+        writer.writerow(conflict)
         
 
