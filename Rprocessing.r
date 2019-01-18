1426b9412f55ae211c2ec365bbf4326515b09718
Nikita Boers <boers.nikita@gmail.com>
2019-01-18 13:46:08 +0100

    finished

--- Rprocessing.r
+++ Rprocessing.r
@@ -10,35 +10,36 @@ ggplot(data = all_conflicts) +
 ggplot(data = all_conflicts) + 
   geom_bar(mapping= aes(x= type_of_conflict, fill= country ), position= "dodge")
 
-facor(as.character(all_conflicts$type_of_conflict))
-
-
-
-# <GEOM_FUNCTION>(
-#  mapping = aes(<MAPPINGS>),
-# stat = <STAT>, 
-#position = <POSITION>
-#) +
-#<COORDINATE_FUNCTION> +
-#<FACET_FUNCTION>
-
-#Making maps of conflicts
+#maps with colorcoding for type of conflict
 library('maps')
+library('ggthemes')
 russia_data <- all_conflicts%>% filter(country%in%'Russia (Soviet Union)')
 russiamap<-map_data('world')%>% filter(region=="Russia")
 ggplot() +
   geom_polygon(data = russiamap, mapping = aes(x=long, y=lat, group=group))+
   geom_point(data = russia_data, mapping= aes(x= longitude ,y= latitude ,color=as.character(type_of_conflict)),alpha=0.4)+ 
-  coord_fixed(1)
+  coord_fixed(1)+theme_map()
   
-
 turkey_data <- all_conflicts%>% filter(country%in%'Turkey')
 turkeymap<-map_data('world')%>%filter(region=="Turkey")
 ggplot() +
   geom_polygon(data = turkeymap, mapping = aes(x=long, y=lat, group=group))+
-  geom_point(data = turkey_data, mapping= aes(x= longitude ,y= latitude ,color=as.character(type_of_conflict)),alpha=0.4)+ coord_fixed()+
+  geom_point(data = turkey_data, mapping= aes(x= longitude ,y= latitude ,color=as.character(type_of_conflict)),alpha=0.4)+ 
   coord_fixed(1)+
   theme_map()
 
-library('ggthemes')
+#maps with colorcoding for years
+ggplot() +
+  geom_polygon(data = russiamap, mapping = aes(x=long, y=lat, group=group))+
+  geom_point(data = russia_data, mapping= aes(x= longitude ,y= latitude ,color= year),alpha=0.4)+ 
+  coord_fixed(1)+
+  theme_map()+
+  scale_color_gradient(low="red", high="yellow")
+
+ggplot() +
+  geom_polygon(data = turkeymap, mapping = aes(x=long, y=lat, group=group))+
+  geom_point(data = turkey_data, mapping= aes(x= longitude ,y= latitude ,color= year),alpha=0.4)+ 
+  coord_fixed(1)+
+  theme_map()+scale_color_gradient(low="red", high="yellow")
+
 
